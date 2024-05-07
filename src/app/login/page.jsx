42635db2e153d5033252115b852c9446c4cdfd29
Kevin <kevxemail@gmail.com>
2024-05-07 17:37:59 -0400

    Made navbar smaller and added a dropshadow

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -19,37 +19,40 @@ export default function Login() {
     };
 
     return (
-        <div style={{ maxWidth: "400px", margin: "0 auto", padding: "20px" }}>
-            <h2>Logi</h2>
-            <form onSubmit={handleLogin}>
-                <div style={{ marginBottom: "15px" }}>
-                    <label htmlFor="email">Email:</label>
-                    <input
-                        type="email"
-                        id="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        placeholder="Enter your email"
-                        required
-                        style={{ width: "100%" }}
-                    />
-                </div>
-                <div style={{ marginBottom: "15px" }}>
-                    <label htmlFor="password">Password:</label>
-                    <input
-                        type="password"
-                        id="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        placeholder="Enter your password"
-                        required
-                        style={{ width: "100%" }}
-                    />
-                </div>
-                <button type="submit" style={{ padding: "10px 20px" }}>
-                    Login
-                </button>
-            </form>
+        <div className="flex flex-col items-center justify-center pt-40">
+            {/* Add a shadow, padding, and border radius */}
+            <div className="bg-white p-10 rounded-lg shadow-lg">
+                <h1 className="text-6xl font-medium p-5">Login</h1>
+                <form onSubmit={handleLogin}>
+                    <div style={{ marginBottom: "15px" }}>
+                        <label htmlFor="email">Email:</label>
+                        <input
+                            type="email"
+                            id="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            placeholder="Enter your email"
+                            required
+                            style={{ width: "100%" }}
+                        />
+                    </div>
+                    <div style={{ marginBottom: "15px" }}>
+                        <label htmlFor="password">Password:</label>
+                        <input
+                            type="password"
+                            id="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            placeholder="Enter your password"
+                            required
+                            style={{ width: "100%" }}
+                        />
+                    </div>
+                    <button type="submit" style={{ padding: "10px 20px" }}>
+                        Login
+                    </button>
+                </form>
+            </div>
         </div>
     );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,17 +4,17 @@ import "../app/styles/globals.css";
 
 const Header = () => {
     return (
-        <div className="flex items-center justify-between bg-customWhiteHeader px-4 py-4 fixed top-0 left-0 w-full z-50">
-            <div className="flex items-center ml-10 h-full py-2 px-5">
+        <div className="flex items-center justify-between bg-customWhiteHeader px-4 py-1 fixed top-0 left-0 w-full z-50 shadow-lg">
+            <div className="flex items-center ml-10 h-full py-1 px-5">
                 {/* Uncomment Image if needed */}
                 <Image
-                    className="mb-2 mr-5"
+                    className="mr-5"
                     src={`/Designer.png`}
                     width="80"
                     height="80"
                     alt="Logo"
                 />
-                <h1 className="text-5xl header-text font-medium font-newsreader">
+                <h1 className="text-5xl header-text font-medium font-newsreader mt-2">
                     Nimbus
                 </h1>
             </div>
@@ -24,21 +24,21 @@ const Header = () => {
                     {/* Reduced spacing */}
                     <li>
                         <Link href="/about-us">
-                            <p className="hover:text-gray-300 py-2 px-5 text-xl">
+                            <p className="hover:text-gray-300 py-3 px-5 text-xl">
                                 About Us
                             </p>
                         </Link>
                     </li>
                     <li>
                         <Link href="/contact-us">
-                            <p className="hover:text-gray-300 py-2 px-5 text-xl">
+                            <p className="hover:text-gray-300 py-3 px-5 text-xl">
                                 Contact Us
                             </p>
                         </Link>
                     </li>
                     <li>
                         <Link href="/login">
-                            <p className="bg-customButton text-xl text-white py-2 px-5 font-bold rounded hover:bg-blue-700 transition-colors text-center ml-5 mr-10">
+                            <p className="bg-customButton text-xl text-white py-3 px-5 font-bold rounded hover:bg-blue-700 transition-colors text-center ml-5 mr-10">
                                 Login
                             </p>
                         </Link>
