c9e667c286903ff2ff805cf6c714648f056b2bd4
Kevin <kevxemail@gmail.com>
2024-05-17 18:10:04 -0400

    test

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { verifyUser } from "@/api/auth";
 import { useRouter } from "next/navigation"; // Redirect to dashboard page
 import Header from "@/components/Header";
@@ -7,6 +7,7 @@ import Dashboard from "@/components/Dashboard";
 
 export default function dashboard() {
     const router = useRouter();
+    const [verified, setVerified] = useState(false);
 
     useEffect(() => {
         const verifyToken = async () => {
@@ -19,6 +20,7 @@ export default function dashboard() {
                 if (!verified) {
                     router.push("/login");
                 }
+                setVerified(true);
             } catch (error) {
                 console.error(error);
                 router.push("/login"); // If there is an error verifying their token don't want them on this page
@@ -30,8 +32,14 @@ export default function dashboard() {
 
     return (
         <>
-            <Header />
-            <Dashboard />
+            {verified ? ( // Only render if there is verification
+                <div>
+                    <Header />
+                    <Dashboard />
+                </div>
+            ) : (
+                <div></div>
+            )}
         </>
     );
 }
